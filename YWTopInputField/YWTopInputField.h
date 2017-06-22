a4b74ad835995251b8b2951dfbbabb61263b0c6a
yoseph_wijaya <info@yoseph.ws>
2017-06-22 13:47:32 +0900

    update initial lib

---
+++ YWTopInputField/YWTopInputField.h
@@ -0,0 +1,19 @@
+//
+//  YWTopInputField.h
+//  YWTopInputField
+//
+//  Created by Yoseph Wijaya on 2017/06/22.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YWTopInputField.
+FOUNDATION_EXPORT double YWTopInputFieldVersionNumber;
+
+//! Project version string for YWTopInputField.
+FOUNDATION_EXPORT const unsigned char YWTopInputFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YWTopInputField/PublicHeader.h>
+
+
