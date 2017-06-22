25119709d8e327c15319a4912575efdf23393784
yoseph_wijaya <info@yoseph.ws>
2017-06-22 18:53:52 +0900

    update controller + test

---
+++ YWTopInputField/Controllers/TopInputFieldController.swift
@@ -0,0 +1,35 @@
+//
+//  TopInputFieldController.swift
+//  YWTopInputField
+//
+//  Created by Yoseph Wijaya on 2017/06/22.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class TopInputFieldController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
