aa3cb0e1f38f9e3dba5b7d06da61e079d9fe694d
yoseph_wijaya <info@yoseph.ws>
2017-06-22 14:45:11 +0900

    update property list

---
+++ YWTopInputField/Controllers/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  YWTopInputField
+//
+//  Created by Yoseph Wijaya on 2017/06/22.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    
+}
---
+++ YWTopInputField/YWTopInputField-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  YWTopInputField-Bridging-Header.h
+//  YWTopInputField
+//
+//  Created by Yoseph Wijaya on 2017/06/22.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+ /* YWTopInputField_Bridging_Header_h */
