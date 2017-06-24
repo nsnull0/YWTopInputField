4a9b5cbc37d9dafb0db5915570227f93c4e3a7bb
yoseph_wijaya <info@yoseph.ws>
2017-06-24 17:35:43 +0900

    add protocol + functionality
    test mode

---
+++ YWTopInputField/Classes/YWInputProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  YWInputProtocol.swift
+//  YWTopInputField
+//
+//  Created by Yoseph Wijaya on 2017/06/24.
+//  Copyright © 2017 Yoseph Wijaya. All rights reserved.
+//
+
+import UIKit
+
+public protocol YWInputProtocol: class {
+    
+    func doneAction(resultStr: String)
+    
+}
--- YWTopInputField/Classes/YWTopInputFieldController.swift
+++ YWTopInputField/Classes/YWTopInputFieldController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 public class YWTopInputFieldController: UIViewController {
 
-    @IBOutlet weak var TempView: UIView!
+    @IBOutlet public weak var containerBlur: UIVisualEffectView!
+    
+    @IBOutlet weak var topContainerBlurConstraint: NSLayoutConstraint!
+    
+    private weak var delegate :YWInputProtocol?
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +28,33 @@ public class YWTopInputFieldController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?, delegate:YWInputProtocol) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.delegate = delegate
     }
-    */
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    
+    
+    //MARK: Animation and Interface
+    public func showInput(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.topContainerBlurConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    public func hideInput (){
+    
+        UIView.animate(withDuration: 0.3, animations: {
+            self.topContainerBlurConstraint.constant = -150
+            self.view.layoutIfNeeded()
+        })
+    }
 
 }
